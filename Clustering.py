5a8333482f755986fe38328dcb32c220d454cb8b
anto1282 <anton.grier@gmail.com>
2023-10-30 10:42:42 +0100

    Wahoo

--- Clustering.py
+++ Clustering.py
@@ -1,76 +1,164 @@
-import numpy as np
-import pandas as pd
-import sys
 
-
-from Bio import pairwise2
-from Bio import SeqIO
-
-
-
-
-assembly = sys.argv[1]
-fileName = sys.argv[2]
-fastQFileIn = sys.argv[3]
-path = sys.argv[4]
-
-DistDict1 = dict()
-ClusterDict = dict()
-#Check the file to see if we have 1 or more contigs
-twoFiles = 0
-for prerecord in SeqIO.parse(assembly,"fasta"):
-    twoFiles += 1
-
-if twoFiles == 1:
-    for record in SeqIO.parse(fastQFileIn,"fastq"):
-        for assemblies in SeqIO.parse(assembly,"fasta"):
-            forwardAlign = pairwise2.align.globalxx(record.seq,assemblies.seq, score_only = True)
-            revAlign = pairwise2.align.globalxx(record.reverse_complement(),assemblies.seq, score_only = True)
-            bestAlignment = max(forwardAlign,revAlign)
-            DistDict1["@"+record.id] = bestAlignment
+def Aligning(aligner, assembly, fastQFileIn):
+    from progress.bar import Bar
+    from Bio import Align, AlignIO,SeqIO
     
-    avgDist = np.mean(np.array(list(DistDict1.values())))
-    Cluster1 =[key for key,value in DistDict1.items() if (value) > avgDist]
-    Cluster2 =[key for key,value in DistDict1.items() if (value) <= avgDist]
-    FastQFile = open(fastQFileIn,"r")
-    OutputFile1 = open(path + "/Clusters_{}/".format(fileName) + fileName+".cluster1.fastq","w")
-    OutputFile2 = open(path + "/Clusters_{}/".format(fileName)  + fileName+".cluster2.fastq","w")
-
-    FastaFlag1 = False
-    FastaFlag2 = False
-    iterator = 0
-
+    AlignDict = dict()
+    seqNr = 0
+    recordSize = 0
+    
     for record in SeqIO.parse(fastQFileIn,"fastq"):
-        if "@"+record.id in Cluster1:
-            print(record.format("fastq"), file = OutputFile1, end="")
-        if "@"+record.id in Cluster2:
-            print(record.format("fastq"), file = OutputFile2, end="")
+        seqNr += 1
+        if recordSize < len(record.seq) and len(record.seq) < 4000:
+            firstRecord = record
+            recordSize = len(firstRecord.seq)
+    
+    bar = Bar('Aligning', max = seqNr, fill = "|")
+    cutoff = 3000
+
+    Alignments_tsv = open("Alignments2_tsv", "w")
+    firstTime = True
+    ReversedFastq = open("HLAC_bin.fastq","w")
+    for record in SeqIO.parse(fastQFileIn,"fastq"):
+        if len(record.seq) > cutoff:
+            for assemblies in SeqIO.parse(assembly,"fasta"):
+                #Use assembly
+                firstRecord = assemblies
+                
+                length = min(len(firstRecord.seq), len(record.seq))
+                forwardAlign =aligner.align(firstRecord.seq[:length],record.seq[2:length-3])
+                revAlign = aligner.align(firstRecord.seq[:length],record.reverse_complement()[2:length-3])
+                if firstTime == True:
+                    print("Reference", "\t", firstRecord.seq, file = Alignments_tsv)
+                    firstTime = False
+                if forwardAlign.score > revAlign.score:
+                    alignment = forwardAlign[0]
+                    break
+                else:
+                    alignment = revAlign[0]
+                    break
+                    
+            print("@"+record.id, "\t", alignment[1,:], file = Alignments_tsv)
+            print()
+            AlignDict["@" + record.id] = alignment[1,:]
+        bar.next()
+    bar.finish()
+    return AlignDict
+
+def fromTSV(fileName):
+    TSV_file = open(fileName)
+    AlignDict = dict()
+    for line in TSV_file:
+        id = line.split("\t")[0].strip()
+        seq = line.split("\t")[1].strip()
+        AlignDict[id] = seq
+    return AlignDict
+
+def Frequency(AlignDict):
+    FreqDict = dict()
+    for key,value in AlignDict.items():
+        for i in range(len(value)):
+            if str(i) in FreqDict:
+                if value[i] in FreqDict[str(i)]:
+                    FreqDict[str(i)][value[i]] += 1
+                else:
+                    FreqDict[str(i)][value[i]] = 1
+            else:
+                FreqDict[str(i)] = dict()
+    return FreqDict
+
+
+def plotit(FreqDict):
+    import matplotlib.pyplot as plt
+    YList = []
+    AList = []
+    GList = []
+    CList = []
+    TList = []
+    NList = []
+    DashList = []
+    #print(FreqDict)
+    for key,value in FreqDict.items():
+        YList.append(key)
+        if "C" in value.keys():
+            CList.append(value["C"])
+        else:
+            CList.append(0)
             
-
-else:
-    for assemblies in SeqIO.parse(assembly,"fasta"):
-        recordnumber = 0
-        bestAlignment = 0
-        for record in SeqIO.parse(fastQFileIn,"fastq"):        
-            recordnumber += 1
-            forwardAlign = pairwise2.align.globalxx(record.seq,assemblies.seq, score_only = True)
-            revAlign = pairwise2.align.globalxx(record.reverse_complement(),assemblies.seq, score_only = True)
-            if max(forwardAlign, revAlign) > bestAlignment:
-                bestAlignment = max(forwardAlign,revAlign)
-                DistDict1["@"+record.id] = [bestAlignment,recordnumber]
-    for i in range(twoFiles):
-        ClusterDict["cluster"+str(i)] = [key for key,value in DistDict1.items() if value[1] == i]
-    for Clusters in ClusterDict.keys():
-        OutputFile = open(path + "/Clusters_{}/".format(fileName) + fileName+".{}.fastq".format(Clusters),"w")
-        for record in SeqIO.parse(fastQFileIn,"fastq"):
-            if "@" + record.id in ClusterDict[Clusters]:
-                print(record.format("fastq"), file = OutputFile, end="")
-
-
+        if "G" in value.keys():
+            GList.append(value["G"])
+        else:
+            GList.append(0)
+            
+        if "A" in value.keys():
+            AList.append(value["A"])
+        else:
+            AList.append(0)
+            
+        if "T" in value.keys():
+            TList.append(value["T"])
+        else:
+            TList.append(0)
+            
+        if "N" in value.keys():
+            NList.append(value["N"])
+        else:
+            NList.append(0)
+            
+        if "-" in value.keys():
+            DashList.append(value["-"])
+        else:
+            DashList.append(0)
+            
+    plt.figure(figsize=(50,10))
+    ListList = [AList,GList,CList,TList, NList]
+    BaseList = ["A","G","C","T","N"]
+    colours = ["green","blue","yellow","red","black"]
+    for i in range(len(ListList)):
+        if len(YList) > len(ListList[i]):
+            for p in range(len(YList)-len(ListList[i])):
+             ListList[i].append(0)
+        plt.subplot(231 + i)
+        plt.plot(YList,ListList[i][:len(YList)], c=colours[i])
+        plt.grid(True)
+        plt.title(BaseList[i], fontsize = 30)
+    plt.savefig("Densities.png")
     
+    nMean = sum(NList)/sum([sum(CList),sum(GList),sum(NList),sum(AList),sum(TList)])
+    return nMean
+
+def main(assembly,fastQFileIn):
+    import numpy as np
+    import pandas as pd
+
+    from Bio import Align, AlignIO,SeqIO
     
+    import sys
+    
+    aligner = Align.PairwiseAligner()
+    aligner.mode = "local"
+
+    aligner.target_left_extend_gap_score = -100
+    aligner.target_left_open_gap_score =  -100
+    aligner.target_right_open_gap_score =  -100
+    aligner.target_right_extend_gap_score = -100
+    aligner.target_internal_open_gap_score = -100
+    aligner.target_internal_extend_gap_score =-20
 
 
+    aligner.match_score = 3
+    aligner.mismatch_score = -2
 
+    aligner.query_right_extend_gap_score = -0.5
+    aligner.query_right_open_gap_score = -1
+    aligner.query_left_open_gap_score = -1
+    aligner.query_left_extend_gap_score = -0.5
+    aligner.query_internal_open_gap_score = -1
+    aligner.query_internal_extend_gap_score = -0.5
+    
+    AlignDict = Aligning(aligner,assembly, fastQFileIn)
+    FreqDict = Frequency(AlignDict)
+    
+    return FreqDict
 
 
